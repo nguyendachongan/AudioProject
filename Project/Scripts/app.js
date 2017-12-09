a14cdc16f54427591d9f125836193cae58bbd776
IMT-SOFT\anndh <anndh@imt-soft.com>
2017-12-09 12:16:31 +0700

    Revert "+ create Project"
    This reverts commit 9c094355eab9b27834098f3e0715640dfe7f3f60.

--- Project/Scripts/app.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.config(['$sceDelegateProvider',
-    function ($sceDelegateProvider) {
-        $sceDelegateProvider.resourceUrlWhitelist(['self']);
-    }
-]);
